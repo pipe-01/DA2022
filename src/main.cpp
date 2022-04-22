1c7b2c79228e14b9dba64b3a2e9cf4e7229fc5c1
Marcos Aires <rafa.aires.13@gmail.com>
2022-04-22 22:33:59 +0100

    path in main.cpp corrected

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
     File file;
 
-    std::vector<Carrinha> carrinhas = file.readAndParseCar("C:\\Users\\ASUS\\PROJETODA\\DA2022\\src\\carrinhas.txt");
-    std::vector<Encomenda> encomendas = file.readAndParseEnc("C:\\Users\\ASUS\\PROJETODA\\DA2022\\src\\encomendas.txt");
+    std::vector<Carrinha> carrinhas = file.readAndParseCar("carrinhas.txt");
+    std::vector<Encomenda> encomendas = file.readAndParseEnc("encomendas.txt");
 
     Menu menu;
     menu.showMenu();
