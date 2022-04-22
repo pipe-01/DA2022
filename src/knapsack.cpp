3268328b9d3281bcac3e1e49f8b89cb3f1664be7
Luísa Araújo <lmpa.pt@gmail.com>
2022-04-22 10:44:09 +0100

    knapsack mais limpinho

--- src/knapsack.cpp
+++ src/knapsack.cpp
@@ -1,11 +1,5 @@
-// Sort the jobs based on their deadlines.
-// Iterate from the end and calculate the available slots between every two consecutive deadlines. Include the profit, deadline, and job ID of ith job in the max heap.
-// While the slots are available and there are jobs left in the max heap, include the job ID with maximum profit and deadline in the result.
-// Sort the result array based on their deadlines.
-
 // Time complexity : O(nlog(n))
 // Space complexity : O(n)
-
 #include "knapsack.h"
 
 
@@ -21,58 +15,15 @@ void sortWorkers(std::vector<Carrinha> *workers){
     // sort(workers->begin(), workers->end(), compareWorkerSize);
 }
 
-// std::vector<Encomenda> testingReq(){
-
-//     Encomenda p1 = Encomenda(50,30,20,1);
-//     Encomenda p2 = Encomenda(50,30,10,1);
-//     Encomenda p3 = Encomenda(50,30,30,1);
-
-//     std::vector<Encomenda> v;
-//     v.push_back(p1);
-//     v.push_back(p2);
-//     v.push_back(p3);
-
-//     return v;
-// }
-
-// std::vector<Carrinha> testingWork(){
-
-//     Carrinha e1 = Carrinha(60,60,14);
-//     Carrinha e2 = Carrinha(20,30,17);
-//     Carrinha e3 = Carrinha(25,10,13);
-
-//     std::vector<Carrinha> v;
-//     v.push_back(e1);
-//     v.push_back(e2);
-//     v.push_back(e3);
-    
-//     return v;
-// }
-
-
-int sol2(std::vector<Encomenda> requests, std::vector<Carrinha> workers){
-
-    unsigned int profit = 0;
-
-
-    for(unsigned int i = 0; i<workers.size(); i++){
-
-    }
-
-    return profit;
-}
 
 int cenario2(std::vector<Encomenda> requests, std::vector<Carrinha> workers){
-
-    // std::vector<Encomenda> requests = testingReq();
-    // std::vector<Carrinha> workers = testingWork();
 
     std::list<std::tuple<Encomenda,Carrinha>> solution;
     std::list<int> save;
 
     
     int profit = 0;
-    bool clean = false;
+    bool clean = true;
 
     sortRequests(&requests);
     sortWorkers(&workers);
@@ -92,9 +43,6 @@ int cenario2(std::vector<Encomenda> requests, std::vector<Carrinha> workers){
     unsigned int i = 0, j = 0;
     int pesoLeft = 0, volLeft = 0;
 
-    //when a request is impossible to carry:
-    int count = 0;
-
     std::vector<std::vector<Encomenda>> sol;
     std::vector<Encomenda> aux;
 
@@ -109,15 +57,11 @@ int cenario2(std::vector<Encomenda> requests, std::vector<Carrinha> workers){
             pesoLeft = workers[i].getPesoMax();
             profit -= workers[i].getCusto();
             clean = false;
-            count++;
         }
 
         if(requests[j].getVolume() <= volLeft && requests[j].getPeso() <= pesoLeft){
-            count = 0;
-            // std::tuple<Encomenda,Carrinha> tup = std::make_tuple(requests[j], workers[i]);
             aux.push_back(requests[j]);
             profit += requests[j].getRecompensa();
-            // solution.push_back(tup);
             pesoLeft -= requests[j].getPeso();
             volLeft -= requests[j].getVolume();
             // std::cout << "i: " << i << ", j: " << j << std::endl;
@@ -132,13 +76,13 @@ int cenario2(std::vector<Encomenda> requests, std::vector<Carrinha> workers){
 
     std::vector<int> nonProfit;
 
-    for(int i = 0; i<sol.size();i++){
-        std::cout << "Encomendas Carinha " << i << ":" << std::endl;
-        for(int j = 0; j<sol[i].size(); j++){
-            std::cout << j << ", ";
-        }
-        std::cout << std::endl;
-    }
+    // for(int i = 0; i<sol.size();i++){
+    //     std::cout << "Encomendas Carinha " << i << ":" << std::endl;
+    //     for(int j = 0; j<sol[i].size(); j++){
+    //         std::cout << j << ", ";
+    //     }
+    //     std::cout << std::endl;
+    // }
 
     // delete non profit workers
     for(int m = 0; m<sol.size(); m++){
@@ -163,15 +107,3 @@ int cenario2(std::vector<Encomenda> requests, std::vector<Carrinha> workers){
     return profit;
 }
 
-//just for testing
-// int main(){
-
-//     std::list<std::tuple<Encomenda,Carrinha>> sol = cenario2();
-
-//     // cout << sol.size() << std::endl;
-
-//     cout << "Solução" << std::endl;
-//     for(auto elem : sol){
-//         cout << "Encomenda: "<< get<0>(elem).getRecompensa() << ", Carrinha:" << get<1>(elem).getCusto() << std::endl;
-//     }
-// }
